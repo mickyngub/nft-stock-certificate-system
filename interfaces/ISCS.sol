183fa2e9c1baaadc93a2d5157c173deb547f58b4
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-01-25 22:31:54 +0700

    add NatSpec comments / remove get functions

--- interfaces/ISCS.sol
+++ interfaces/ISCS.sol
@@ -1,50 +1,67 @@
 // SPDX-License-Identifier: MIT
 
-// MultiSigWallet functions and explanations are from @gnosis/MultiSigWallet
 pragma solidity ^0.8.10;
+
+/// @title Interface for Stock Certificate System
+/// @dev MultiSigWallet functions and explanations are from @gnosis/MultiSigWallet
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 interface ISCS is IERC721 {
+  /// @dev MultiSigWallet's event
   event Confirmation(address indexed sender, uint256 indexed transactionId);
   event Revocation(address indexed sender, uint256 indexed transactionId);
   event Submission(uint256 indexed transactionId);
   event Execution(uint256 indexed transactionId);
   event ExecutionFailure(uint256 indexed transactionId);
   event Deposit(address indexed sender, uint256 value);
-
+  /// @dev Stock NFT ERC721's event
   event StockReissuing(uint256 amount);
   event StockIssuing(uint256 amount);
 
+  /// @dev MultiSigWallet's functions
+
   function MultiSigWallet(address[] _owners, uint256 _required) public;
 
+  /// @dev Allows an owner to submit and confirm a transaction.
+  /// @param destination Transaction target address.
+  /// @param value Transaction ether value.
+  /// @param data Transaction data payload.
+  /// @return Returns transaction ID.
   function submitTransaction(
     address destination,
     uint256 value,
     bytes32 data
   ) public returns (uint256 transactionId);
 
+  /// @dev Allows an owner to confirm a transaction.
+  /// @param transactionId Transaction ID.
   function confirmTransaction(uint256 transactionId) public;
 
+  /// @dev Allows an owner to revoke a confirmation for a transaction.
+  /// @param transactionId Transaction ID.
   function revokeConfirmation(uint256 transactionId) public;
 
+  /// @dev Allows anyone to execute a confirmed transaction.
+  /// @param transactionId Transaction ID.
   function executeTransaction(uint256 transactionId) public;
 
+  /// @dev Returns the confirmation status of a transaction.
+  /// @param transactionId Transaction ID.
+  /// @return Confirmation status.
   function isConfirmed(uint256 transactionId) public pure returns (bool);
 
+  /*
+   * Internal functions
+   */
+  /// @dev Adds a new transaction to the transaction mapping, if transaction does not exist yet.
+  /// @param destination Transaction target address.
+  /// @param value Transaction ether value.
+  /// @param data Transaction data payload.
+  /// @return Returns transaction ID.
   function addTransaction(
     address destination,
     uint256 value,
     bytes32 data
   ) internal returns (uint256 transactionId);
-
-  function getConfirmationCount(uint256 transactionId)
-    public
-    pure
-    returns (uint256 count);
-
-  function getTransactionCount(bool pending, bool executed)
-    public
-    pure
-    returns (uint256 count);
 }
